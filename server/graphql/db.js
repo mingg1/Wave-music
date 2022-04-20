98072c643cf7fc7d0359d063568ccd635c3e5ef0
Minji Choi <comg2843@gmail.com>
2022-04-20 12:42:06 +0300

    hide api key

--- server/graphql/db.js
+++ server/graphql/db.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const BASE_URL = 'https://ws.audioscrobbler.com/2.0/';
-const ALBUM_INFO_URL = `${BASE_URL}?format=json&api_key=2d1938e40bbc2ae14ee1fde1ccd061d7&method=chart.gettoptracks`;
+const ALBUM_INFO_URL = `${BASE_URL}?format=json&api_key=${process.env.LAST_FM_API_KEY}&method=chart.gettoptracks`;
 
 export const getTopTracks = async () => {
   const {
@@ -10,14 +13,14 @@ export const getTopTracks = async () => {
       tracks: { track },
     },
   } = await axios(ALBUM_INFO_URL);
-  const [obj1, obj2] = await axios.all([
-    axios.get('https://api.sampleapis.com/beers/ale'),
-    axios.get('https://api.sampleapis.com/coffee/hot'),
-  ]);
+  // const [obj1, obj2] = await axios.all([
+  //   axios.get('https://api.sampleapis.com/beers/ale'),
+  //   axios.get('https://api.sampleapis.com/coffee/hot'),
+  // ]);
 
   const getLink = async () => {
     const result = await axios.get(
-      'https://www.last.fm/music/5+Seconds+of+Summer/Youngblood+(Deluxe)/Want+You+Back'
+      'https://www.last.fm/music/Harry+Styles/_/Adore+You'
     );
 
     const $ = cheerio.load(result.data);
