15f016df1b61fb65e2177df4086c71efec3275e6
Minji Choi <comg2843@gmail.com>
2022-04-22 04:30:31 +0300

    encrypt password before saving to db

--- server/models/User.js
+++ server/models/User.js
@@ -2,13 +2,19 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    required: true,
+    unique: true,
+  },
+  nickname: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
 userSchema.pre('save', async function () {
-  this.password = await bcrypt.hash(this.password, 5);
+  this.password = await bcrypt.hash(this.password, 12);
 });
 
 const User = mongoose.model('User', userSchema);
