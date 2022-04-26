2926858f87ab21be424260cc6d20b4fd6dcad4e4
Minji Choi <comg2843@gmail.com>
2022-04-26 13:57:06 +0300

    added link states

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom';
 import '../App.css';
 import TokenContext from '../contexts/token-context';
 import Input from '../components/UI/Input/Input';
-import Button from '../components/UI/Button/Button';
+//import Button from '../components/UI/Button/Button';
 import AuthContext from '../contexts/auth-context';
+import { Button, Menu, MenuItem, Fade } from '@mui/material';
 
 const GET_PL = gql`
   {
@@ -15,6 +16,8 @@ const GET_PL = gql`
       images {
         url
       }
+      description
+      collaborative
     }
   }
 `;
@@ -34,23 +37,79 @@ const Home = () => {
     // update or not
   }, [error]);
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    console.log(event.currentTarget);
+  };
+  const handleClose = (event) => {
+    console.log(event.currentTarget.innerText);
+    setAnchorEl(null);
+  };
+
   return (
     <div className="App">
       <form>
+        <div>
+          <Button
+            id="fade-button"
+            aria-controls={open ? 'fade-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+          >
+            Dashboard
+          </Button>
+          <Menu
+            id="fade-menu"
+            MenuListProps={{
+              'aria-labelledby': 'fade-button',
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+          >
+            <MenuItem onClick={handleClose}>track</MenuItem>
+            <MenuItem onClick={handleClose}>album</MenuItem>
+            <MenuItem onClick={handleClose}>artist</MenuItem>
+            <MenuItem onClick={handleClose}>all</MenuItem>
+          </Menu>
+        </div>
         <Input placeholder="Search for" />
-        <Button type="submit">Search</Button>
+        <Button type="submit" variant="outlined">
+          Search
+        </Button>
       </form>
 
       <p> {(loading || error) && 'Loading..'} </p>
       <div>
         {data &&
           data.featuredPaylists.map((pl) => {
+            console.log(pl);
             return (
               <div key={pl.id}>
-                <Link id={pl.id} to={`/playlist/${pl.id}`}>
+                <Link
+                  id={pl.id}
+                  to={`/playlist/${pl.id}`}
+                  state={{
+                    name: pl.name,
+                    description: pl.description,
+                    coverImg: pl.images[0].url,
+                  }}
+                >
                   <img src={pl.images[0].url} style={{ width: 140 }} />
                 </Link>
-                <Link id={pl.id} to={`/playlist/${pl.id}`}>
+                <Link
+                  id={pl.id}
+                  to={`/playlist/${pl.id}`}
+                  state={{
+                    name: pl.name,
+                    description: pl.description,
+                    coverImg: pl.images[0].url,
+                  }}
+                >
                   <h5>{pl.name}</h5>
                 </Link>
               </div>
