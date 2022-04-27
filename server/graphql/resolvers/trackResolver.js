2aad2bb26284d9771d4cabc1216f071d65becd41
Minji Choi <comg2843@gmail.com>
2022-04-28 02:53:59 +0300

    added recommendations of track

--- server/graphql/resolvers/trackResolver.js
+++ server/graphql/resolvers/trackResolver.js
@@ -2,22 +2,28 @@ export default {
   Favorite: {
     tracks: async (parent, args, { dataSources, req }) => {
       const { sf_token } = req.headers;
-
       const ids = parent.tracks?.filter((t) => t).toString() || args.ids;
-
       return await dataSources.spotifyAPI.getTracks(sf_token, ids);
     },
   },
   Query: {
     tracks: async (parent, { ids }, { dataSources, req }) => {
       const { sf_token } = req.headers;
-
       // const ids = parent.tracks?.toString() || args.ids;
       return await dataSources.spotifyAPI.getTracks(sf_token, ids);
     },
-    recommendations: async (_, { id }, { dataSources, req }) => {
+    recommendations: async (
+      _,
+      { seedArtists, seedGenres, seedTracks },
+      { dataSources, req }
+    ) => {
       const { sf_token } = req.headers;
-      return await dataSources.spotifyAPI.getRecommendations(sf_token, id);
+      return await dataSources.spotifyAPI.getRecommendations(
+        sf_token,
+        seedArtists,
+        seedGenres,
+        seedTracks
+      );
     },
   },
 };
