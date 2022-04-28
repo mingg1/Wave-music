7f8c9bafb6926e09c978452ae50d64c6248e5307
Minji Choi <comg2843@gmail.com>
2022-04-28 15:35:51 +0300

    simplified code

--- client/src/routes/PlayListDetail.js
+++ client/src/routes/PlayListDetail.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { gql, useMutation, useQuery } from '@apollo/client';
-import { Typography } from '@mui/material';
-import LikeButton from '../components/LikeButton';
 import { useLocation, useParams, Link } from 'react-router-dom';
 import TokenContext from '../contexts/token-context';
-import { Bars } from 'react-loader-spinner';
+
+import PlaylistHeader from '../components/PlaylistHeader';
+import TrackCard from '../components/TrackCard';
+import LoadingIcon from '../components/LoadingIcon';
 
 const GET_PL_TRACKS = gql`
   query playlistTracks($playlistId: ID!) {
@@ -22,6 +23,9 @@ const GET_PL_TRACKS = gql`
         album {
           id
           name
+          images {
+            url
+          }
         }
       }
     }
@@ -33,8 +37,7 @@ const PlayListDetail = () => {
 
   const { id } = useParams();
   const loggedInUser = JSON.parse(localStorage.getItem('user')) || null;
-  const { fetchToken, userFavorites, setUserFavorites } =
-    useContext(TokenContext);
+  const { fetchToken, userFavorites } = useContext(TokenContext);
 
   const { loading, data, error } = useQuery(GET_PL_TRACKS, {
     variables: { playlistId: id },
@@ -64,60 +67,24 @@ const PlayListDetail = () => {
     if (!loading && error) {
       fetchToken();
     }
-    console.log(userFavorites);
   }, [error, userFavorites]);
 
   return (
     <>
-      {(loading || error) && (
-        <Bars
-          heigth="100"
-          width="100"
-          color="grey"
-          ariaLabel="loading-indicator"
-        />
-      )}
+      {(loading || error) && <LoadingIcon />}
       {data && (
         <>
-          <div style={{ display: 'flex' }}>
-            <img src={location.state.coverImg} style={{ width: '20vw' }} />
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-              <Typography component="h1" variant="h2">
-                {location.state.name}
-              </Typography>
-              <Typography component="h5" variant="h5">
-                {location.state.description}
-              </Typography>
-            </div>
-          </div>
+          <PlaylistHeader element={location?.state} />
           <div>
             {data &&
               data.playlistTracks.map((t) => {
                 const { track } = t;
-
                 return (
-                  <div
+                  <TrackCard
                     key={track.id}
-                    style={{ backgroundColor: 'tomato', marginBottom: 10 }}
-                  >
-                    {loggedInUser && (
-                      <LikeButton
-                        trackId={track.id}
-                        type={track.type}
-                        userId={loggedInUser.id}
-                        isLiked={userFavorites?.tracks
-                          ?.map((f) => f.id)
-                          ?.includes(track.id)}
-                      />
-                    )}
-
-                    <h5>{track.name}</h5>
-                    {track?.artists?.map((artist) => (
-                      <Link id={artist.id} to={`/artist/${artist.id}`}>
-                        {artist.name} {artist.id}
-                      </Link>
-                    ))}
-                  </div>
+                    track={track}
+                    favorites={userFavorites}
+                  />
                 );
               })}
           </div>
