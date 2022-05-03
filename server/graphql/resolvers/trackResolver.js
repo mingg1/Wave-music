0ca07b6867675b9bf82e37010f5609550e4da583
Minji Choi <comg2843@gmail.com>
2022-05-04 02:14:19 +0300

    get more track info from backend

--- server/graphql/resolvers/trackResolver.js
+++ server/graphql/resolvers/trackResolver.js
@@ -2,15 +2,42 @@ export default {
   UserPlaylist: {
     tracks: async (parent, args, { dataSources, req }) => {
       const { sf_token } = req.headers;
-      const ids = parent.tracks?.filter((t) => t).toString() || args.ids;
-      return await dataSources.spotifyAPI.getTracks(sf_token, ids);
+      const PlTracks = parent.tracks;
+      if (PlTracks.length > 20) {
+        const trackDataList = new Array();
+        const arrayAmount = Math.ceil(PlTracks.length / 20);
+        for (let i = 0; i < arrayAmount; i++) {
+          const splitedTracks = PlTracks.splice(-20).toString();
+          trackDataList.push(
+            await dataSources.spotifyAPI.getTracks(sf_token, splitedTracks)
+          );
+        }
+        return trackDataList.flat();
+      } else {
+        const ids = parent.tracks?.filter((t) => t).toString() || args.ids;
+        return await dataSources.spotifyAPI.getTracks(sf_token, ids);
+      }
     },
   },
   Favorite: {
     tracks: async (parent, args, { dataSources, req }) => {
       const { sf_token } = req.headers;
-      const ids = parent.tracks?.filter((t) => t).toString() || args.ids;
-      return await dataSources.spotifyAPI.getTracks(sf_token, ids);
+      const favTracks = parent.tracks;
+      if (favTracks.length > 20) {
+        const trackDataList = new Array();
+        const arrayAmount = Math.ceil(favTracks.length / 20);
+
+        for (let i = 0; i < arrayAmount; i++) {
+          const splitedTracks = favTracks.splice(-20).toString();
+          trackDataList.push(
+            await dataSources.spotifyAPI.getTracks(sf_token, splitedTracks)
+          );
+        }
+        return trackDataList.flat();
+      } else {
+        const ids = parent.tracks?.filter((t) => t).toString() || args.ids;
+        return await dataSources.spotifyAPI.getTracks(sf_token, ids);
+      }
     },
   },
   Query: {
